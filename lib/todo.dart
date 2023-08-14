76ef8350d69ff6cb6fa86a0e353ee73983c17fa0
Maple06 <malendra.sahla.rizky@gmail.com>
2023-08-14 09:49:38 +0700

    Add Todo Page - Tugas 7 Agustus 2023 FIN

--- lib/todo.dart
+++ lib/todo.dart
@@ -8,6 +8,16 @@ class TodoApp extends StatefulWidget {
 }
 
 class _TodoAppState extends State<TodoApp> {
+  List<String> todo = <String>[
+    'Code App',
+    'Do Dishes',
+    'Go for a run',
+    'Buy roses',
+    'Make tutorial',
+    'Flutter boy'
+  ];
+  List<bool> isChecked = [false, false, false, false, false, false];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,6 +26,56 @@ class _TodoAppState extends State<TodoApp> {
         centerTitle: true,
         automaticallyImplyLeading: false,
         backgroundColor: Colors.grey[700],
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            // SizedBox(height:20),
+            Expanded(
+                child: ListView.builder(
+                    padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 12),
+                    itemCount: todo.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Container(
+                        height: 80,
+                        margin: EdgeInsets.all(10),
+                        decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: Colors.brown[300]),
+                        child: Row(
+                          children: [
+                            SizedBox(width:15),
+                            Checkbox(
+                              value: isChecked[index],
+                              onChanged: (bool? newValue) {
+                                setState(() {
+                                  if (newValue != null) {
+                                    isChecked[index] = newValue;
+                                  }
+                                });
+                              },
+                            ),
+                            Text(
+                              '${todo[index]}',
+                              style: TextStyle(
+                                  decoration: isChecked[index] ? TextDecoration.lineThrough : TextDecoration.none,
+                              ),
+                            )
+                          ],
+                        ),
+                      );
+                    })),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          setState(() {
+            todo.add('New Task');
+            isChecked.add(false);
+          });
+        },
+        label: const Text('Add'),
+        icon: const Icon(Icons.add),
+        backgroundColor: Colors.brown[700],
       ),
     );
   }
